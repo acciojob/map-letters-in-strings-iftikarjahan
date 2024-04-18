d831d85fa52210b4609c9cf8293c49f67db48343
iftikarjahan <69801895+iftikarjahan@users.noreply.github.com>
2024-04-18 15:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function mapLetters(str){
+	
+	let obj={};
+for(let i=0;i<str.length;i++){
+  if(!obj.hasOwnProperty(str[i])){
+    obj[str[i]]=[i]
+  }
+  else{
+    let arr=obj[str[i]];
+    arr.push(i);
+  }
+}
+
+return obj;
+}
+
+module.exports = mapLetters
